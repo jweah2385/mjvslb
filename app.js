15a908ef81a7194774578145b7cec51ed4aa43f7
James Weah <jweah@aggies.ncat.edu>
2023-07-06 16:04:45 -0400

    feat: Push trail api information to server page with ejs template

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+app.set('view engine', 'ejs');
 const https = require('https');
 
+let track = '';
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  const temp = res.sendFile(__dirname + '/index.html');
-});
+  const stats =
+    'https://www.balldontlie.io/api/v1/stats?player_ids[]=237&seasons[]=2008';
 
-app.post('/', function (req, res) {
-  'https://www.balldontlie.io/api/v1/stats?player_ids[]=237&seasons[]=2008';
-
-  const first_name = req.https.get(url, function (response) {
+  let data = '';
+  https.get(stats, function (response) {
     console.log(response.statusCode);
 
-    let nbaStats = '';
-    var name = req.body.first_name;
-    response.on('data', function (data) {
-      nbaStats += data;
+    response.on('data', function (chunk) {
+      data += chunk;
     });
 
     response.on('end', function () {
-      nbaStats = JSON.parse(nbaStats);
-      console.log(nbaStats);
-      const SpnbaStats = nbaStats.data[23].player.first_name;
-      console.log('here is the stat' + SpnbaStats);
-      console.log(name);
-      res.send('LBJ Trial: ' + SpnbaStats);
+      let nbaStats = JSON.parse(data);
+      // let lbppg = nbaStats.data[0].team.full_name;
+      let points = nbaStats.data[0].pts;
+      console.log(points);
+      track = points;
+      res.render('mjvlb', { lbStats: track });
     });
   });
 });
+
+app.post('/', function (req, res) {});
 
 //app.post('/', function (request, response) {});
 
